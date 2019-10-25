27384a27e19d0b73b7be6117654dfb4e05dc4b82
Valéria Cavalcanti <valeria.cavalcanti@ifpb.edu.br>
2019-10-24 23:36:30 -0300

    Correção da questão 05, Lista 01

--- Gabarito/Exercícios/Lista01/questao05.py
+++ Gabarito/Exercícios/Lista01/questao05.py
@@ -1,7 +1,7 @@
-menor = maior = int(input("Número: "))
+menor = maior = float(input("Número: "))
 
-for i in range(1, 10):
-	num = int(input("Número: "))
+for i in range(1, 100):
+	num = float(input("Número: "))
 	if (num < menor):
 		menor = num
 	elif (num > maior):
